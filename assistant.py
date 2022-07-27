61d871fb08a50648b02c1ffebe8e6144b56dd6b1
Nytri <llTheBlankll@gmail.com>
2022-07-27 20:07:50 +0800

    Configuration Update and some progress with comments

--- assistant.py
+++ assistant.py
@@ -1,6 +1,6 @@
 import spacy
 import pyttsx3
-from sympy import re
+import json
 import miscellaneous.Terminal
 
 import speech_recognition as sr
@@ -16,7 +16,12 @@ class Assistant:
   def say(self, message) -> None:
     self.voice_engine.say(message);
       
-  def listen(self) -> None:
+  def listen(self) -> str:
+    """Listen to microphone and return the voice as a sentence.
+
+    Returns:
+        voice_to_text: Returns your voice as a sentence.
+    """
     try:
       with sr.Microphone() as microphone:
         # Set the ambient noise to increase the range of recongizing the audio.
@@ -31,6 +36,15 @@ class Assistant:
       self.say("I didn't understand what you just said.")
       pass
     
-  def execute_voice_command(self, voice_message) -> None:
+  def execute_voice_command(self, voice_message: str) -> None:
+    """Execute voice command that's in voice_commands.json
+
+    Args:
+        voice_message (str): This is what you said to your assistant (e.x: 'open visual studio code')
+    """
     with open("./config/voice_commands.json", "r") as voice_commands:
-      pass
+      voice_commands_name: dict = json.load(voice_commands)
+      eval_code: str
+      for voice_command_name in voice_commands_name:
+        eval_code = voice_command_name["command"]
+        
